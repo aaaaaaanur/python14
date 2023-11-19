21ac578365ecbaa16026cc769403a9b699d894ba
Алена <alena@Sergeis-MacBook-Pro.local>
2023-11-19 23:40:15 +0500

    task01

---
+++ blabla.py
@@ -0,0 +1,8 @@
+a = [1, 2, 3]
+
+print(a)
+
+for i in a:
+    i = 100
+
+print(a)
---
+++ task01.py
@@ -0,0 +1,21 @@
+# a = 20, b = 24
+def summ(a, b):
+    list1 = list(range(a, b + 1))
+    # list1 = [20, 21, 22, 23, 24]
+    sum2 = 0
+    for i in list1:
+        l = []
+        for y in str(i):
+            y2 = int(y)
+            l.append(y2)
+        if len(set(l)) == len(l):
+            sum2 = sum2 + i
+    return sum2
+
+
+print(summ(20, 24))
+
+
+
+
+
